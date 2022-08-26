f2049eb202810a37152d3d27093296fc23b4bf70
Fantantonio <antoniopanfili@gmail.com>
2022-08-26 08:59:29 +0200

    From fun to class component (enable inheritance)

--- progetto_adw_cs/src/App.js
+++ progetto_adw_cs/src/App.js
@@ -1,21 +1,23 @@
+import { Component } from "react";
 import "../node_modules/bootstrap/dist/js/bootstrap";
 import Navbar from './components/Navbar';
 import TestTable from './components/TestTable';
 import './styles/App.css';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <div className="App">
       <Navbar />
-      <hr class="m-0"></hr>
+      <hr className="m-0"></hr>
       
-      <div class="container mt-4">
+      <div className="container mt-4">
 
-        <div class="text-center my-4">
-          <button class="btn btn-lg btn-primary me-1 mb-1">
+        <div className="text-center my-4">
+          <button className="btn btn-lg btn-primary me-1 mb-1">
             Nuovo Test
           </button>
-          <button class="btn btn-lg btn-primary ms-1 mb-1">
+          <button className="btn btn-lg btn-primary ms-1 mb-1">
             Nuova Domanda
           </button>
         </div>
@@ -23,7 +25,7 @@ function App() {
         <TestTable />
       </div>
     </div>
-  );
+  );}
 }
 
 export default App;
--- progetto_adw_cs/src/components/TestTable.js
+++ progetto_adw_cs/src/components/TestTable.js
@@ -1,59 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 
-function TestTable() {
-    return (
-        <div class="card">
-            <h5 class="card-header">Lista dei test</h5>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-hover table-sm">
-                        <thead>
-                            <tr>
-                            <th scope="col" class="text-start">#</th>
-                            <th scope="col" class="text-start">Data</th>
-                            <th scope="col" class="text-center">Nome</th>
-                            <th scope="col" class="text-center">Ordine Casuale</th>
-                            <th scope="col" class="text-center">Domande Numerate</th>
-                            <th scope="col" class="text-end">Inizia il test</th>
-                            </tr>
-                        </thead>
-                        <tbody class="table-group-divider">
-                            <tr>
-                            <th class="text-start" scope="row">1</th>
-                            <td class="text-start">2022-01-12</td>
-                            <td class="text-center">Esame 1</td>
-                            <td class="text-center">false</td>
-                            <td class="text-center">true</td>
-                            <td class="text-end">
-                                <a href="#">Inizia</a>
-                            </td>
-                            </tr>
-                            <tr>
-                            <th class="text-start" scope="row">2</th>
-                            <td class="text-start">2022-06-04</td>
-                            <td class="text-center">Esame 2</td>
-                            <td class="text-center">false</td>
-                            <td class="text-center">false</td>
-                            <td class="text-end">
-                                <a href="#">Inizia</a>
-                            </td>
-                            </tr>
-                            <tr>
-                            <th class="text-start" scope="row">3</th>
-                            <td class="text-start">2022-08-25</td>
-                            <td class="text-center">Esame 3</td>
-                            <td class="text-center">true</td>
-                            <td class="text-center">false</td>
-                            <td class="text-end">
-                                <a href="#">Inizia</a>
-                            </td>
-                            </tr>
-                        </tbody>
-                    </table>
+class TestTable extends Component {
+    render() {
+        return (
+            <div className="card">
+                <h5 className="card-header">Lista dei test</h5>
+                <div className="card-body">
+                    <div className="table-responsive">
+                        <table className="table table-hover table-sm">
+                            <thead>
+                                <tr>
+                                <th scope="col" className="text-start">#</th>
+                                <th scope="col" className="text-start">Data</th>
+                                <th scope="col" className="text-center">Nome</th>
+                                <th scope="col" className="text-center">Ordine Casuale</th>
+                                <th scope="col" className="text-center">Domande Numerate</th>
+                                <th scope="col" className="text-end">Inizia il test</th>
+                                </tr>
+                            </thead>
+                            <tbody className="table-group-divider">
+                                <tr>
+                                <th className="text-start" scope="row">1</th>
+                                <td className="text-start">2022-01-12</td>
+                                <td className="text-center">Esame 1</td>
+                                <td className="text-center">false</td>
+                                <td className="text-center">true</td>
+                                <td className="text-end">
+                                    <a href="#">Inizia</a>
+                                </td>
+                                </tr>
+                                <tr>
+                                <th className="text-start" scope="row">2</th>
+                                <td className="text-start">2022-06-04</td>
+                                <td className="text-center">Esame 2</td>
+                                <td className="text-center">false</td>
+                                <td className="text-center">false</td>
+                                <td className="text-end">
+                                    <a href="#">Inizia</a>
+                                </td>
+                                </tr>
+                                <tr>
+                                <th className="text-start" scope="row">3</th>
+                                <td className="text-start">2022-08-25</td>
+                                <td className="text-center">Esame 3</td>
+                                <td className="text-center">true</td>
+                                <td className="text-center">false</td>
+                                <td className="text-end">
+                                    <a href="#">Inizia</a>
+                                </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default TestTable
