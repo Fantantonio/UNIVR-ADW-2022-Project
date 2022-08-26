63ec9223d6ea1923b6d449955ead6f12b96f3fc3
eric2296 <bb.eric22@gmail.com>
2022-08-26 15:08:27 +0200

    Modify CORS Policy

--- progetto_adw/src/main/java/dev/fantantonio/progetto_adw/ProgettoAdwApplication.java
+++ progetto_adw/src/main/java/dev/fantantonio/progetto_adw/ProgettoAdwApplication.java
@@ -2,9 +2,24 @@ package dev.fantantonio.progetto_adw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+@CrossOrigin("http://localhost:3000") // to allow from all domains
 @SpringBootApplication
 public class ProgettoAdwApplication {
+
+@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgettoAdwApplication.class, args);
