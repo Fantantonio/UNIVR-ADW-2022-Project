52e06cc8212b8323f0a056c909e0a26edaaeca43
Fantantonio <antoniopanfili@gmail.com>
2022-08-28 22:01:16 +0200

    Setup Apollo, add Query in node (Eric)

---
+++ progetto_adw_cs/src/gql/Query.js
@@ -0,0 +1,33 @@
+import { gql } from "@apollo/client";
+
+
+export const GET_ALL_RISPOSTE = gql`
+query GetRisposte{
+    tutteRisposte {
+      id
+    }
+  } 
+`
+;
+
+export const GET_ALL_TEST = gql`
+query GetTest{
+  tuttiTest{
+    data,
+    nome,
+    ordinecasuale,
+    domandeconnumero
+  }
+}
+`;
+
+export const GET_ALL_DOMANDE = gql`
+query GetDomande{
+  tutteDomande{
+   nome,
+   testo,
+   punti,
+   ordinecasuale,
+   risposteconnumero     
+ }
+}`;
