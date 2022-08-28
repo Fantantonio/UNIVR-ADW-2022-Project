f14ed6354fd6e6d4a813cf67190599869a041a7c
eric2296 <bb.eric22@gmail.com>
2022-08-28 15:38:35 +0200

    Update ProgettoAdwApplication.java

--- progetto_adw/src/main/java/dev/fantantonio/progetto_adw/ProgettoAdwApplication.java
+++ progetto_adw/src/main/java/dev/fantantonio/progetto_adw/ProgettoAdwApplication.java
@@ -2,10 +2,30 @@ package dev.fantantonio.progetto_adw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+
 
 @SpringBootApplication
 public class ProgettoAdwApplication {
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
+    }
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProgettoAdwApplication.class, args);
 	}
