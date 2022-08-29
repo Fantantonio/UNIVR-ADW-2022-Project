a4f43f1c417ee76e1c95d98e4b47c2cab997967f
eric2296 <bb.eric22@gmail.com>
2022-08-29 22:21:38 +0200

    Updated Mutation for Tests

--- progetto_adw/src/main/java/dev/fantantonio/progetto_adw/controller/TestController.java
+++ progetto_adw/src/main/java/dev/fantantonio/progetto_adw/controller/TestController.java
@@ -1,13 +1,18 @@
 package dev.fantantonio.progetto_adw.controller;
+import org.springframework.graphql.data.method.annotation.Argument;
 
+import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import dev.fantantonio.progetto_adw.model.Test;
 import dev.fantantonio.progetto_adw.repository.TestRepository;
+//import net.bytebuddy.asm.Advice.Argument;
 
 @Controller
 @RequestMapping("/test")
@@ -23,4 +28,13 @@ public class TestController {
 	public List<Test> findAll() {
 		return testRepository.findAll();
 	}
+	
+	//@MutationMapping
+	@SchemaMapping(typeName = "Mutation", value = "addTest")
+		Test addTest(@Argument TestInput test) {
+			Test t = new Test(test.data(), test.nome(),test.domandeconnumero, test.ordinecasuale());
+			return testRepository.save(t);
+		}
+		record TestInput(String data, String nome, Boolean domandeconnumero,boolean ordinecasuale) {}
+
 }
