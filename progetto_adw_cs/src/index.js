bf3b322f09e54efa275173d4f7dd09fb64add69b
Fantantonio <antoniopanfili@gmail.com>
2022-08-31 11:53:15 +0200

    Remove unused files

--- progetto_adw_cs/src/index.js
+++ progetto_adw_cs/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ApolloClient,InMemoryCache, ApolloProvider } from "@apollo/client"
 
 const client = new ApolloClient({
@@ -10,17 +9,11 @@ const client = new ApolloClient({
   cache: new InMemoryCache()
 });
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <ApolloProvider client={client}>
       <App />
-    </React.StrictMode>
-  </ApolloProvider>
+    </ApolloProvider>
+  </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
