cbab04efd09eeb4a9aa9690a1dc5165a1857911f
Fantantonio <antoniopanfili@gmail.com>
2022-08-31 23:58:34 +0200

    Refactor class to function

--- progetto_adw_cs/client/src/components/PaginaFineTest.js
+++ progetto_adw_cs/client/src/components/PaginaFineTest.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react'
 import RiassuntoDomandaRisposta from './RiassuntoDomandaRisposta'
 
-class PaginaFineTest extends Component {
+const PaginaFineTest = ({setPage, userRole}) => {
 
-  render() {
-    return (
-      <>
-      <div className="card my-4">
-          <div className="card-body">
-              <RiassuntoDomandaRisposta />
-              <hr />
-              <RiassuntoDomandaRisposta />
-              <hr />
-              <RiassuntoDomandaRisposta />
-              <hr />
-              <RiassuntoDomandaRisposta />
-              <hr />
+  return (
+    <>
+    <div className="card my-4">
+        <div className="card-body">
+            <RiassuntoDomandaRisposta />
+            <hr />
+            <RiassuntoDomandaRisposta />
+            <hr />
+            <RiassuntoDomandaRisposta />
+            <hr />
+            <RiassuntoDomandaRisposta />
+            <hr />
 
-              <button className="btn btn-success">Torna alla pagina iniziale</button>
-          </div>
-      </div>
-      </>
-    )
-  }
+            <button className="btn btn-success">Torna alla pagina iniziale</button>
+        </div>
+    </div>
+    </>
+  )
 }
 
 export default PaginaFineTest
